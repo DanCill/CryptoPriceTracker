bd87bb8bfea19c88dded7db6fc313f3c5928505a
Dan Cilliers <dan.cilliers@hailer.com>
2024-07-27 11:31:47 +0300

    Added crypto service from coinGecko, added chart functionality

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -1,33 +1,59 @@
-// import { View, Text, Image } from 'react-native'
-// import React from 'react'
-// import { ChartDot, ChartPath, ChartPathProvider } from '@rainbow-me/animated-charts';
+import { View, Text, Image, Dimensions } from "react-native";
+import React from "react";
+import { LineChart } from "react-native-wagmi-charts";
 
-// const Chart = ({ currentPrice, logoUrl, name, priceChangePercentage7d, sparkline, symbol }) => {
+const deviceWidth = Dimensions.get("window").width;
+const deviceHeight = Dimensions.get("window").height;
 
-//     const priceChangeColor =
-//     priceChangePercentage7d > 0 ? "text-[#34C759]" : "text-[#FF3B30]";
+const Chart = ({
+  currentPrice,
+  logoUrl,
+  name,
+  priceChangePercentage7d,
+  sparkline,
+  symbol,
+}) => {
+  const priceChangeColor =
+    priceChangePercentage7d > 0 ? "text-[#34C759]" : "text-[#FF3B30]";
 
-//   return (
-//     <ChartPathProvider data={{ points:sparkline, smoothingStrategy: 'bezier' }} >
-//             <View className="m-6" >
-//       <View className="">
-//         <View className="flex-row justify-between items-center">
-//             <View className="flex-row items-center" >
-//                 <Image source={{uri: logoUrl}} className="w-[24px] h-[24px] mr-2"/>
-//                 <Text className="text-[14px] text-[#A9ABB1]" >{name} ({symbol.toUpperCase()})</Text>
-//             </View>
-//             <Text className="text-[14px] text-[#A9ABB1]" >7d</Text>
-//         </View>
-//         <View className="flex-row justify-between items-center">
-//             <Text className="text-[24px] text-black font-bold">${currentPrice.toLocaleString('en-US', { currency: 'USD'})}</Text>
-//             <Text className={`text-[18px] ${priceChangeColor}`}>{priceChangePercentage7d.toFixed(2)}%</Text>
-//         </View>
-//       </View>
-//       <ChartPath height={SIZE / 2} stroke="yellow" width={SIZE} />
-//       <ChartDot style={{ backgroundColor: 'blue' }} />
-//     </View>
-//     </ChartPathProvider>
-//   )
-// }
+  return (
+    <LineChart.Provider data={sparkline}>
+      <View className="m-6">
+        <View className="">
+          <View className="flex-row justify-between items-center">
+            <View className="flex-row items-center">
+              <Image
+                source={{ uri: logoUrl }}
+                className="w-[24px] h-[24px] mr-2"
+              />
+              <Text className="text-[14px] text-[#A9ABB1]">
+                {name} ({symbol.toUpperCase()})
+              </Text>
+            </View>
+            <Text className="text-[14px] text-[#A9ABB1]">7d</Text>
+          </View>
+          <View className="flex-row justify-between items-center">
+            <Text className="text-[24px] text-black font-bold">
+              ${currentPrice.toLocaleString("en-US", { currency: "USD" })}
+            </Text>
+            <Text className={`text-[18px] ${priceChangeColor}`}>
+              {priceChangePercentage7d.toFixed(2)}%
+            </Text>
+          </View>
+        </View>
+      </View>
+      <LineChart
+        className="mx-3 justify-center items-center"
+        width={deviceWidth - 25}
+        height={deviceHeight * 0.25}
+      >
+        <LineChart.Path />
+        <LineChart.CursorCrosshair color="red">
+          <LineChart.HoverTrap />
+        </LineChart.CursorCrosshair>
+      </LineChart>
+    </LineChart.Provider>
+  );
+};
 
-// export default Chart
+export default Chart;
---
+++ services/cryptoService.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import moment from "moment";
+
+const formatSparkline = (numbers) => {
+  const sevenDaysAgo = moment().subtract(7, "days").unix();
+  let formattedSparkline = numbers.map((item, index) => {
+    return {
+      timestamp: sevenDaysAgo + (index + 1) * 3600,
+      value: item,
+    };
+  });
+  return formattedSparkline;
+};
+const formatMarketData = (data) => {
+  let formattedData = [];
+  data.forEach((item) => {
+    const formattedSparkLine = formatSparkline(item.sparkline_in_7d.price);
+    const formattedItem = {
+      ...item,
+      sparkline_in_7d: {
+        price: formattedSparkLine,
+      },
+    };
+
+    formattedData.push(formattedItem);
+  });
+  return formattedData;
+};
+
+export const getMarketData = async () => {
+  try {
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&sparkline=true&price_change_percentage=7d",
+      {
+        headers: {
+          accept: "application/json",
+          "x-cg-demo-api-key": "API_KEY_HERE",
+        },
+      }
+    );
+    const data = response.data;
+    const formattedResponse = formatMarketData(data);
+    return formattedRespoanse;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
